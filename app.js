3daab8be8f58182542cdb6c180ed90a8ccf8d9db
ERLEY <erley.1701920254@ucaldas.edu.co>
2023-02-09 09:20:26 -0500

    Se agrega app.js con funcion de resta y el archivo .gitignore

---
+++ app.js
@@ -0,0 +1,16 @@
+let n1 = 7;
+let n2 = 3;
+
+/**
+ * Funcion que permite restar dos numeros
+ * Los parametros son los dos numeros
+ * y retorna el resultado de la resta de los dos numeros
+ */
+
+function restar(num1, num2){
+    let resultado = num1 - num2;
+    console.log("El resultado de la resta es ${resultado}");
+    return resultado;
+}
+
+restar(n1,n2);
