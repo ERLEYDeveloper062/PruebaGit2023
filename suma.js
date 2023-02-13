58e80a6b06638c252b41f003a9f89ae657ed5abe
ERLEY <erley.1701920254@ucaldas.edu.co>
2023-02-13 10:53:38 -0500

    primera versión de las funciones

---
+++ suma.js
@@ -0,0 +1,16 @@
+let n1 = 7;
+let n2 = 3;
+
+/**
+ * Funcion que permite restar dos numeros
+ * Los parametros son los dos numeros
+ * y retorna el resultado de la resta de los dos numeros
+ */
+
+function sumar(num1, num2){
+    let resultado = num1 + num2;
+    console.log("El resultado de la resta es ${resultado}");
+    return resultado;
+}
+
+sumar(n1,n2);
